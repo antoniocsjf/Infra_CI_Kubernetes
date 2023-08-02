5223720a50661b89fcc65f6426e34bea999090e9
antoniocsjf <128857078+antoniocsjf@users.noreply.github.com>
2023-08-02 10:22:58 -0300

    Update Provider.tf

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -16,7 +16,7 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  load_config_file       = false
+  
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
